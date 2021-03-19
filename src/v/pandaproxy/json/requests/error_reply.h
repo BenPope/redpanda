330996fa4100b54dac2f79d3990e02923606a313
Ben Pope <ben@vectorized.io>
2021-03-19 03:03:53 +0000

    pp/json: Introduce map_error_code
    Mapping from kafka::error_code to ppj::error_code
    
    Signed-off-by: Ben Pope <ben@vectorized.io>

--- src/v/pandaproxy/json/requests/error_reply.h
+++ src/v/pandaproxy/json/requests/error_reply.h
@@ -12,6 +12,8 @@
 #pragma once
 
 #include "json/json.h"
+#include "kafka/protocol/errors.h"
+#include "pandaproxy/json/error.h"
 #include "seastarx.h"
 
 #include <seastar/core/sstring.hh>
@@ -23,6 +25,19 @@ struct error_body {
     ss::httpd::reply::status_type error_code;
     ss::sstring message;
 };
+
+inline error_code map_error_code(kafka::error_code ec) {
+    switch (ec) {
+    case kafka::error_code::unknown_topic_or_partition:
+        return error_code::partition_not_found;
+    case kafka::error_code::unknown_member_id:
+        return error_code::consumer_instance_not_found;
+    case kafka::error_code::offset_out_of_range:
+        return error_code::unprocessable_entity;
+    default:
+        return error_code::kafka_error;
+    }
+}
 
 inline void rjson_serialize(
   rapidjson::Writer<rapidjson::StringBuffer>& w, const error_body& v) {
