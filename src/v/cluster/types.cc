8776dc1a480c09d7021bda5203bf518724ad5d9c
Evgeny Lazin <evgeny@vectorized.io>
2021-12-29 13:16:04 -0500

    cluster: Convert duration to ms correctly
    Use duration_cast instead of using count() method.

--- src/v/cluster/types.cc
+++ src/v/cluster/types.cc
@@ -545,7 +545,8 @@ void adl<model::timeout_clock::duration>::to(iobuf& out, duration dur) {
     // This is a clang bug that cause ss::cpu_to_le to become ambiguous
     // because rep has type of long long
     // adl<rep>{}.to(out, dur.count());
-    adl<uint64_t>{}.to(out, dur.count());
+    adl<uint64_t>{}.to(
+      out, std::chrono::duration_cast<std::chrono::milliseconds>(dur).count());
 }
 
 model::timeout_clock::duration
