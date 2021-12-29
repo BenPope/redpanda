60250442b3ad3060732eeae2b01b159277b4caff
Evgeny Lazin <evgeny@vectorized.io>
2021-12-29 13:04:51 -0500

    archival: Convert duration to milliseconds properly
    Use std::chrono::duration_cast instead of relying on
    std::chrono::duration<...>::count.

--- src/v/archival/types.cc
+++ src/v/archival/types.cc
@@ -14,10 +14,16 @@
 
 namespace archival {
 
+static uint64_t to_milliseconds(ss::lowres_clock::duration interval) {
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(interval);
+    return ms.count();
+}
+
 std::ostream&
 operator<<(std::ostream& o, const std::optional<segment_time_limit>& tl) {
     if (tl) {
-        fmt::print(o, "{}", tl.value()().count());
+        auto ms = to_milliseconds(tl.value()());
+        fmt::print(o, "{}", ms);
     } else {
         fmt::print(o, "N/A");
     }
@@ -31,10 +37,10 @@ std::ostream& operator<<(std::ostream& o, const configuration& cfg) {
       "segment_upload_timeout: {}, "
       "manifest_upload_timeout: {}, time_limit: {}}}",
       cfg.bucket_name,
-      cfg.interval.count(),
-      cfg.initial_backoff.count(),
-      cfg.segment_upload_timeout.count(),
-      cfg.manifest_upload_timeout.count(),
+      to_milliseconds(cfg.interval),
+      to_milliseconds(cfg.initial_backoff),
+      to_milliseconds(cfg.segment_upload_timeout),
+      to_milliseconds(cfg.manifest_upload_timeout),
       cfg.time_limit);
     return o;
 }
