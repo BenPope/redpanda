1c6eb05bffe3355626bbd121dc9cf6ee12a92c3b
Aaron Fabbri <fabbri@redpanda.com>
2022-07-21 19:14:11 -0700

    Aliases for named semaphores
    Introduce some helpful definitions for making named semaphores standard. These
    have minimal overhead and include a name in exception messages for better
    debuggability.

---
+++ src/v/ssx/semaphore.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2022 Redpanda Data, Inc.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+#pragma once
+
+#include <seastar/core/semaphore.hh>
+#include <seastar/core/sstring.hh>
+
+#include <utility>
+
+namespace ssx {
+
+// We use named semaphores because the provided name will be included in
+// exception messages, making diagnoising broken or timed-out semaphores much
+// easier.
+using semaphore_units
+  = seastar::semaphore_units<seastar::named_semaphore_exception_factory>;
+
+template<typename T = seastar::sstring>
+seastar::named_semaphore make_semaphore(size_t count, T&& name) {
+    return {
+      count, seastar::named_semaphore_exception_factory{std::forward<T>(name)}};
+}
+
+} // namespace ssx
