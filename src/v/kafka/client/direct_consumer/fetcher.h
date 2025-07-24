db296582852b8caaecd75b8e1ea94c9976d87617
Ben Pope <ben@redpanda.com>
2025-07-24 16:52:42 +0100

    k/client/fetcher: Limit to api_version 12
    Topic Identifiers need explciit support.
    
    Signed-off-by: Ben Pope <ben@redpanda.com>

--- src/v/kafka/client/direct_consumer/fetcher.h
+++ src/v/kafka/client/direct_consumer/fetcher.h
@@ -150,6 +150,7 @@ private:
  * - support incremental fetches
  * - support leader epochs
  * - support server side throttling and quotas
+ * - support topic identifiers
  */
 class fetcher {
 public:
